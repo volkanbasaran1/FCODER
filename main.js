9edfa9b3f3623514441652eb4c75703a58789add
volkanbasaran1 <volkanba12@gmail.com>
2023-09-12 14:18:34 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const header = document.querySelector("header");
+
+
+
+const menuBtn=document.querySelector('#menu-icon')
+
+const navbar=document.querySelector('.navbar')
+
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("sticky", window.scrollY > 60);
+});
+
+
+menuBtn.addEventListener('click',()=>{
+    navbar.classList.toggle('open')
+})
